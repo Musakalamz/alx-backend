7b7b83f8b134cc45da76d41bff14a226d56b7523
Musakalamz <ogunsolumusa@gmail.com>
2024-10-31 04:22:09 +0000

    LFU Caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,121 +1,66 @@
 #!/usr/bin/env python3
-""" Basic Cache implementing LFU """
-# imports
-BasicCache = __import__('0-basic_cache').BasicCache
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
 
 
-class CacheItem:
-    """ Implementation of a cache item """
-    def __init__(self, key, value):
-        """ Cache Item """
-        self.key = key
-        self.value = value
+class LFUCache(BaseCaching):
+    """
+    FIFOCache defines a FIFO caching system
+    """
 
-class LFUCacheItem(CacheItem):
-    """ Least Frequently Used Inherits from CacheItem """
-    def __init__(self, key, value, age, freq):
-        """ Constructor """
-        super().__init__(key, value)
-        self.age = age
-        self.freq = freq
-
-    def updateItem(self, value):
-        """ Update a cache item """
-        self.value = value
-        self.age = 0
-        self.freq += 1
-
-class LFUCache(BasicCache):
-    """ LFU (Least Recently Used) implementation of BasicCache """
     def __init__(self):
-        """ Constructor """
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
-        self.LFU = []
-    
+        self.usage = []
+        self.frequency = {}
+
     def put(self, key, item):
-        """ Put new item in cache assoc. with key """
-        # Do nothing if key is NoneType or item is NoneType
-        if (key is None or item is None):
-            return
-
-        # If Key exists, update Cache Item
-        if key in self.cache_data:
-            for ci in self.LFU:
-                if ci.key is key:
-                    ci.updateItem(item)
+        """
+        Cache a key-value pair
+        """
+        if key is None or item is None:
+            pass
         else:
-            # Length will be longer than max capacity, make room
-            if len(self.cache_data) == self.MAX_ITEMS:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                lfu = min(self.frequency.values())
+                lfu_keys = []
+                for k, v in self.frequency.items():
+                    if v == lfu:
+                        lfu_keys.append(k)
+                if len(lfu_keys) > 1:
+                    lru_lfu = {}
+                    for k in lfu_keys:
+                        lru_lfu[k] = self.usage.index(k)
+                    discard = min(lru_lfu.values())
+                    discard = self.usage[discard]
+                else:
+                    discard = lfu_keys[0]
 
-                # Determine discarded cache item
-                discard = self.LFU[0]
-                for x in self.LFU:
-                    if x.freq < discard.freq or \
-                        x.freq == discard.freq and x.age > discard.age:
-                        discard = x
-
-                # Discard Cache Item
-                print("DISCARD: {}".format(discard.key))
-                del self.cache_data[discard.key]
-                self.LFU.remove(discard)
-
-            # Add new Cache Item
-            data = LFUCacheItem(key, item, 0, 0)
-            self.LFU.append(data)
-
-        # increase age of all items
-        for x in self.LFU:
-            x.age += 1
-
-        self.cache_data[key] = item
+                print("DISCARD: {}".format(discard))
+                del self.cache_data[discard]
+                del self.usage[self.usage.index(discard)]
+                del self.frequency[discard]
+            # update usage frequency
+            if key in self.frequency:
+                self.frequency[key] += 1
+            else:
+                self.frequency[key] = 1
+            if key in self.usage:
+                del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get item from cache """
-        if key is None or key not in self.cache_data:
-            return None
-        else:
-            for x in self.LFU:
-                if x.key == key:
-                    x.age = 0
-                    x.freq += 1
-                else:
-                    x.age += 1
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
+            del self.usage[self.usage.index(key)]
+            self.usage.append(key)
+            self.frequency[key] += 1
             return self.cache_data[key]
-
-if __name__ == "__main__":
-    my_cache = LFUCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    print(my_cache.get("B"))
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    print(my_cache.get("A"))
-    print(my_cache.get("B"))
-    print(my_cache.get("C"))
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
-    my_cache.put("G", "San Francisco")
-    my_cache.print_cache()
-    my_cache.put("H", "H")
-    my_cache.print_cache()
-    my_cache.put("I", "I")
-    my_cache.print_cache()
-    print(my_cache.get("I"))
-    print(my_cache.get("H"))
-    print(my_cache.get("I"))
-    print(my_cache.get("H"))
-    print(my_cache.get("I"))
-    print(my_cache.get("H"))
-    my_cache.put("J", "J")
-    my_cache.print_cache()
-    my_cache.put("K", "K")
-    my_cache.print_cache()
-    my_cache.put("L", "L")
-    my_cache.print_cache()
-    my_cache.put("M", "M")
-    my_cache.print_cache()
+        return None
