94fa2bf6eea0bb6bbff9a5d0aa9337b172862fe0
Musakalamz <ogunsolumusa@gmail.com>
2024-10-31 04:32:21 +0000

    FIFO Caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,60 +1,40 @@
 #!/usr/bin/env python3
-""" Basic Cache implementing FIFO """
-# imports
-BasicCache = __import__('0-basic_cache').BasicCache
+""" BaseCaching module
+"""
+from base_caching import BaseCaching
 
 
-class CacheItem:
-    """ Implementation of a cache item """
-    def __init__(self, key, value):
-        """ Cache Item """
-        self.key = key
-        self.value = value
+class FIFOCache(BaseCaching):
+    """
+    FIFOCache defines a FIFO caching system
+    """
 
-class FIFOCache(BasicCache):
-    """ FIFO (First In Last Out) implementation of BasicCache """
     def __init__(self):
-        """ Constructor """
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
-        self.FIFO = []
+        self.order = []
 
     def put(self, key, item):
-        """ Put new item in cache assoc. with key """
-        if (key is None or item is None):
-            return
-
-        if (key in self.cache_data):
-            self.FIFO = [ci for ci in self.FIFO if ci.key != key]
-
-        self.cache_data[key] = item
-
-        data = CacheItem(key, item)
-        # data = {"name": key, "item": item}
-
-        self.FIFO.append(data)
-
-        if len(self.cache_data) > self.MAX_ITEMS:
-            print("DISCARD: {}".format(self.FIFO[0].key))
-            del self.cache_data[self.FIFO[0].key]
-            self.FIFO.pop(0)
+        """
+        Cache a key-value pair
+        """
+        if key is None or item is None:
+            pass
+        else:
+            length = len(self.cache_data)
+            if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                print("DISCARD: {}".format(self.order[0]))
+                del self.cache_data[self.order[0]]
+                del self.order[0]
+            self.order.append(key)
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get item from cache """
-        if key is None or key not in self.cache_data:
-            return None
-        else:
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data.keys():
             return self.cache_data[key]
-
-if __name__ == "__main__":
-    my_cache = FIFOCache()
-    my_cache.put("A", "Hello")
-    my_cache.put("B", "World")
-    my_cache.put("C", "Holberton")
-    my_cache.put("D", "School")
-    my_cache.print_cache()
-    my_cache.put("E", "Battery")
-    my_cache.print_cache()
-    my_cache.put("C", "Street")
-    my_cache.print_cache()
-    my_cache.put("F", "Mission")
-    my_cache.print_cache()
+        return None
